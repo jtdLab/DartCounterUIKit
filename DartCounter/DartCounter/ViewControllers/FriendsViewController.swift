93088ee2afb203727d1567d42478341ff7f69508
Jonas Schlauch <mrjosch99@gmail.com>
2020-12-26 02:04:53 +0100

    show friends

--- DartCounter/DartCounter/ViewControllers/FriendsViewController.swift
+++ DartCounter/DartCounter/ViewControllers/FriendsViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class FriendsViewController: UIViewController {
     
-    @IBOutlet weak var friendsTableView: UITableView!
-    var items = ["ONLINE SPIELER 1", "ONLINE SPIELER 2"]
+    @IBOutlet weak var tableView_friends: ContentSizedTableView!
+    var friends: [String]?
     
     @IBAction func onAddFriend(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: Segues.Friends_SearchUser, sender: self)
@@ -18,30 +18,44 @@ class FriendsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        initView()
-    }
-    
-    private func initView() {
-        // register FriendCell to FriendsTable
-        friendsTableView.register(UINib(nibName: "FriendCell", bundle: nil), forCellReuseIdentifier: "FriendCell")
         
-        friendsTableView.dataSource = self
-        friendsTableView.delegate = self
-        friendsTableView.isScrollEnabled = false
+        // register FriendCell to FriendsTable
+        tableView_friends.register(UINib(nibName: "FriendCell", bundle: nil), forCellReuseIdentifier: "FriendCell")
+        
+        tableView_friends.dataSource = self
+        tableView_friends.delegate = self
+        tableView_friends.isScrollEnabled = false
+        
+        UserService.delegate = self
+        
+        guard let user = UserService.user else { return }
+        onUserChanged(user: user)
     }
 
+}
+
+
+// handle events from UserService
+extension FriendsViewController: UserServiceDelegate {
+    
+    func onUserChanged(user: User) {
+        self.friends = user.friends
+    
+        tableView_friends.reloadData()
+    }
+    
 }
 
 
 extension FriendsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return friends?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as! FriendCell
-        cell.label_name.text = items[indexPath.row]
+        cell.label_name.text = friends?[indexPath.row]
         return cell
     }
 }
